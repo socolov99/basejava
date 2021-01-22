eb0d245bcfc755aa6a3b22c2a37e891bb444b1c3
socolov99 <socolov99@gmail.com>
2021-01-23 01:43:56 +0300

    NotExistStorageException super()

--- src/com/urise/webapp/exception/NotExistStorageException.java
+++ src/com/urise/webapp/exception/NotExistStorageException.java
@@ -3,6 +3,6 @@ package com.urise.webapp.exception;
 public class NotExistStorageException extends StorageException {
 
     public NotExistStorageException(String uuid) {
-        super("Resume " + uuid + " not exist", uuid);
+        super("Resume " + uuid + " doesn't exist", uuid);
     }
 }
