99ec45089dbfde160e5e09e67390d378d015c8f7
socolov99 <socolov99@gmail.com>
2021-01-29 18:37:05 +0300

    homework7 ContactType

---
+++ src/com/urise/webapp/model/ContactType.java
@@ -0,0 +1,16 @@
+package com.urise.webapp.model;
+
+public enum ContactType {
+    PHONE("Тел."),
+    SKYPE("Skype:"),
+    EMAIL("Почта:");
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
