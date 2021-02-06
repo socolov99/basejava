2fe54083508768047ef5f774e66bc336b72ff9ce
socolov99 <socolov99@gmail.com>
2021-02-06 22:49:47 +0300

    Lesson08 MainFile create recursion

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -1,30 +1,23 @@
 package com.urise.webapp;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Objects;
 
 public class MainFile {
     public static void main(String[] args) {
-        String filePath = ".gitignore";
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalFile());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-        File dir = new File("src/com/urise/webapp");
-        System.out.println(dir.isDirectory());
-        System.out.println(dir.isFile());
-        for (String name : Objects.requireNonNull(dir.list())) {
-            System.out.println(name);
-        }
+        String filePath = "/Users/kirillsokolov/Desktop/course_basejava/basejava";
+        File rootFile = new File(filePath);
+        findFile(rootFile);
 
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            System.out.println(fileInputStream.read());
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+    private static void findFile(File rootFile){
+        for (File file  : Objects.requireNonNull(rootFile.listFiles())) {
+            if (file.isDirectory()){
+                findFile(file);
+            }
+            else {
+                System.out.println(file.getName());
+            }
         }
     }
 }
