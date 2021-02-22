677162ac9764fcc7df4d9fc5819009fed04389c6
socolov99 <socolov99@gmail.com>
2021-02-23 00:10:29 +0300

    Lesson 11

---
+++ src/com/urise/webapp/LazySingleton.java
@@ -0,0 +1,28 @@
+package com.urise.webapp;
+
+public class LazySingleton {
+//    double sin;
+    private static volatile LazySingleton INSTANCE;
+
+    private LazySingleton() {
+    }
+
+    private static class LazySingletonHolder{
+        private static final LazySingleton INSTANCE= new LazySingleton();
+    }
+
+    public static LazySingleton getInstance(){
+        return LazySingletonHolder.INSTANCE;
+    }
+//    public static LazySingleton getInstance() {
+//        if (INSTANCE == null) {
+//            synchronized (LazySingleton.class) {
+//                if (INSTANCE == null) {
+//                    sin = 1;
+//                    INSTANCE = new LazySingleton();
+//                }
+//            }
+//        }
+//        return INSTANCE;
+//    }
+}
