b8c0d77cc5fa1a07bd1cd0344db5ef86bf6f4846
socolov99 <socolov99@gmail.com>
2021-02-23 22:35:26 +0300

    DeadLockExample.class: remove main method into the beginning of the class

--- src/com/urise/webapp/DeadLockExample.java
+++ src/com/urise/webapp/DeadLockExample.java
@@ -1,8 +1,25 @@
 package com.urise.webapp;
 
+import java.util.Objects;
+
 public class DeadLockExample {
     private static final String LOCK_1 = "LOCK_1";
     private static final String LOCK_2 = "LOCK_2";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread0 = new Thread(() -> threadRun(LOCK_1, LOCK_2));
+        Thread thread1 = new Thread(() -> threadRun(LOCK_2, LOCK_1));
+
+        thread0.start();
+        thread1.start();
+
+        Thread.sleep(1000);
+
+        if (thread0.isAlive() && thread1.isAlive()) {
+            System.out.println("It's deadlock");
+        }
+    }
 
     private static void threadRun(String lock1, String lock2) {
         System.out.println(Thread.currentThread().getName() + " is waiting for " + lock1 + " holding");
@@ -21,22 +38,5 @@ public class DeadLockExample {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
 
-        Thread thread0 = new Thread(() -> {
-            threadRun(LOCK_1, LOCK_2);
-        });
-        Thread thread1 = new Thread(() -> {
-            threadRun(LOCK_2, LOCK_1);
-        });
-
-        thread0.start();
-        thread1.start();
-
-        Thread.sleep(1000);
-
-        if (thread0.isAlive() && thread1.isAlive()) {
-            System.out.println("It's deadlock");
-        }
-    }
 }
