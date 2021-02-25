12f65d27a478b1d48558d66deeeee18f7beb93d6
socolov99 <socolov99@gmail.com>
2021-02-25 13:44:16 +0300

    refactor methods minValue(int []) and oddOrEven(List<Integer>) with only one return statement

--- src/com/urise/webapp/StreamsHomeWork.java
+++ src/com/urise/webapp/StreamsHomeWork.java
@@ -5,34 +5,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.lang.Math.pow;
-
 public class StreamsHomeWork {
     public static void main(String[] args) {
         int[] values = {2, 4, 2, 6, 3, 4, 8, 6, 5, 8, 4, 2, 4, 6, 9, 5, 6, 9, 1};
         System.out.println(minValue(values));
 
-        List<Integer> integers = Arrays.asList(1, 4, 2, 3, 1);
+        List<Integer> integers = Arrays.asList(1, 4, 2, 3, 1, 1);
         System.out.println(oddOrEven(integers));
     }
 
     private static int minValue(int[] values) {
-        int[] array = IntStream.of(values).distinct().sorted().toArray();
-        int size = array.length;
-        int min = 0;
-        for (int element : array) {
-            min += pow(10, size - 1) * element;
-            size--;
-        }
-        return min;
+        return Arrays.stream(IntStream.of(values).distinct().sorted().toArray()).reduce(0, (left, right) -> left * 10 + right);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        int sum = integers.stream().reduce(Integer::sum).get();
-        if (sum % 2 == 0) {
-            return integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList());
-        } else {
-            return integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
-        }
+        return integers.stream().reduce(0, Integer::sum) % 2 == 0 ?
+                integers.stream().filter(x -> x % 2 != 0).collect(Collectors.toList()) :
+                integers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
     }
 }
