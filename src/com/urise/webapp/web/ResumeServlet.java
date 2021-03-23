90b327f01e0bbb6fbe4386d00f1dc327cf9ed193
socolov99 <socolov99@gmail.com>
2021-03-23 23:23:28 +0300

    Lesson 15

---
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String name = request.getParameter("name");
+        if (name == null) {
+            response.getWriter().write("Hello resumes");
+        } else {
+            response.getWriter().write("Hello " + name + " !");
+        }
+    }
+}
