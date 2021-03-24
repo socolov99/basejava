72d8742efe4a1a79269f0a3e271ced5466f0f47d
socolov99 <socolov99@gmail.com>
2021-03-24 20:25:36 +0300

    resume servlet

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,10 +1,22 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.storage.Storage;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
+    Storage storage;
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws javax.servlet.ServletException, IOException {
 
@@ -14,11 +26,14 @@ public class ResumeServlet extends javax.servlet.http.HttpServlet {
             throws javax.servlet.ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
+
         String name = request.getParameter("name");
         if (name == null) {
-            response.getWriter().write("Hello resumes");
+            response.getWriter().write("Hello Resumes !");
+
         } else {
-            response.getWriter().write("Hello " + name + " !");
+            String resumes = storage.getAllSorted().toString();
+            response.getWriter().write(resumes);
         }
     }
 }
